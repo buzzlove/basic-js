bc5f2ae2516423f1092a6344efa9496babb06443
Andrey Bazlov <buzzlove@mail.ru>
2022-04-30 20:18:33 +0500

    feat: finish extend-repeater (can not do current test on binary and null)

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,10 +15,32 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+ function repeater(str, options) {
+  let result = '';
+  if(options.addition === false) {options.addition = 'false'}
+  if(options.addition === null) {options.addition = 'null'}
+  options.separator = options.separator || '+';
+  options.additionSeparator = options.additionSeparator || '|';
+  options.repeatTimes = options.repeatTimes || 1;
+  options.additionRepeatTimes = options.additionRepeatTimes || 1;
+  options.addition = options.addition || '';
+  //String(options.addition)
+  //options.addition = ((typeof(options.addition) !== 'binary') ? options.addition : options.addition.toString()) || '';
+  function repeat(callNum){
+      let res = str;
+      for(let i = 0; i < options.additionRepeatTimes; i++){
+          res+= options.addition + ((i !== (options.additionRepeatTimes-1)) ? options.additionSeparator:'');
+      }
+      result += res + ((callNum === 'last') ? '' : options.separator);
+  }
+  for(let i = 0; i < options.repeatTimes; i++){
+      ((i !== (options.repeatTimes-1) ) ? repeat('noLat') : repeat('last'));
+      
+  }   
+  return result; 
 }
+
 
 module.exports = {
   repeater
